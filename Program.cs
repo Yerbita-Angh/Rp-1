36f63f6aed3857218f7b6e93fdeba791653196e9
Yerbita-Angh <alejandronicolasgarcia01@gmail.com>
2024-09-05 09:46:40 -0500

    Rp-1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese la cantidad de variables");
+
+int N = int.Parse(Console.ReadLine());
+int contador = 0;
+for (int i = 0; i < N; i++) {
+    Console.WriteLine("Ingrese una variable");
+    int num = int.Parse(Console.ReadLine());
+    if (num >= 1000) {
+        contador++;
+    }
+}
+Console.WriteLine($"La cantidad de valores mayor igual a 1000 es {contador}");
